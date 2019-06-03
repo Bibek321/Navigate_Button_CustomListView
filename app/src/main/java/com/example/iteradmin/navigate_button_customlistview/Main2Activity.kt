e6a5571ca321860656067ba19cbe0092f3b149b7
iteradmin <bibekchakrabarty98@gmail.com>
2019-06-03 19:04:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/iteradmin/navigate_button_customlistview/Main2Activity.kt
@@ -0,0 +1,49 @@
+package com.example.iteradmin.navigate_button_customlistview
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class Main2Activity : BaseActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun getItemId(): Int {
+        return R.id.second
+    }
+
+    override fun getContentView(): Int {
+        return R.layout.activity_main2
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(getItemId()){
+            R.id.first -> {
+                startActivity(Intent(this,MainActivity::class.java))
+                return true
+            }
+            R.id.second -> {
+                startActivity(Intent(this,Main2Activity::class.java))
+                return true
+            }
+            R.id.third -> {
+                startActivity(Intent(this,Main3Activity::class.java))
+                return true
+            }
+
+        }
+        return false
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        val btnv=findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        btnv.setOnNavigationItemSelectedListener(this)
+
+    }
+}
